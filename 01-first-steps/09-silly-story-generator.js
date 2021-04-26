ead546aaa3135c43172bc21b4cc35a8761c1ae06
jatwing <jatwing@outlook.com>
2021-04-26 22:40:01 +0800

    silly story generator

--- 01-first-steps/09-silly-story-generator.js
+++ 01-first-steps/09-silly-story-generator.js
@@ -33,8 +33,7 @@ function result() {
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
-  newStory = newStory.replace(':insertx:', xItem);
-  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(/:insertx:/g, xItem);
   newStory = newStory.replace(':inserty:', yItem);
   newStory = newStory.replace(':insertz:', zItem);
 
