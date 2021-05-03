c60e6f6470365827b483ec653b6e91efe6633340
jatwing <jatwing@outlook.com>
2021-05-03 23:46:26 +0800

    object-oriented-js

--- 03-objects/02-object-oriented-js.js
+++ 03-objects/02-object-oriented-js.js
@@ -7,20 +7,39 @@ function Person(first, last, age, gender, interests) {
   this.gender = gender;
   this.interests = interests;
   this.bio = function () {
-    let pronoun = 'He'
+    let pronoun = 'He';
     if (this.gender === 'female') {
-      pronoun = 'She'
+      pronoun = 'She';
     }
-    const temp = this.interests.slice()
-    temp[temp.length - 1] = 'and ' + temp[temp.length - 1]
-    console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.` + ` ${pronoun} likes ${temp.join(', ')}.`)
-
+    let interestsString = '';
+    for (let i = 0; i < interests.length; i++) {
+      if (i === 0) {
+        interestsString += interests[i];
+      } else if (i === interests.length - 1) {
+        interestsString += ' and ' + interests[i];
+      } else {
+        interestsString += ', ' + interests[i];
+      }
+    }
+    console.log(
+      `${this.name.first} ${this.name.last} is ${this.age} years old.` +
+        ` ${pronoun} likes ${interestsString}.`
+    );
   };
   this.greeting = function () {
-    console.log("Hi! I'm " + this.name.first + ".");
+    console.log(`Hi! I'm ${this.name.first}.`);
   };
 }
-let person1 = new Person("Bob", "Smith", 32, "male", ["music", "skiing"]);
+let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
 
-console.log(person1["age"], person1.interests[1]);
+console.log(person1['age'], person1.interests[1]);
 person1.bio();
+
+
+let person2 = Object.create(person1);
+console.log(person2.name)
+person2.greeting()
+
+
+
+
